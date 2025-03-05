14d0e0328ab9458420417789dcc0b0f3ccc7d755
Noelle <noelearns@gmail.com>
2025-03-04 17:14:55 -0800

    "Added arrays and function"

--- mm.js
+++ mm.js
@@ -1,3 +1,9 @@
-cons sayings =['let that be the source of your energy.', 
-                'You are the one that will beat this horrible thing.', 
-                'this is all for you.']
+const action =["run", "jump", "swim", "dance", "sing", "write", "read", "eat", "sleep", "drive"]; 
+const adjective = ["happy", "bright", "creative", "calm", "energetic", "peaceful", "colorful", "brave", "kind", "mysterious"];
+const noun = ["book", "mountain", "ocean", "computer", "flower", "city", "dog", "phone", "tree", "car"];
+const date = ["today", "tomorrow", "next week", "next month"];
+
+const mixedMessage = () => 
+    console.log('Hi');
+
+mixedMessage();
