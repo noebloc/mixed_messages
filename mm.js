e38c8188d4fc774f68e6a509876637aca9dae6d6
Noelle <noelearns@gmail.com>
2025-03-11 17:37:48 -0700

    Added If condition and button names

--- mm.js
+++ mm.js
@@ -3,33 +3,28 @@ const adjective = ["happy", "bright", "creative", "calm", "energetic", "peaceful
 const noun = ["book", "mountain", "ocean", "computer", "flower", "city", "dog", "phone", "tree", "car"];
 const date = ["today", "tomorrow", "next week", "next month"];
 
-let verb = action[Math.floor(Math.random() * 10)];
-let adj = adjective[Math.floor(Math.random() * 10)];
-let nou = noun[Math.floor(Math.random() * 10)];
-let da = date[Math.floor(Math.random() * 4)]
-
 let value = 0;
 
 let buttonClick = document.getElementById('buttonClicker');
 
-function mixMini(){
-    document.getElementById("wacky").innerHTML = `You will ${verb} ${adj} ${nou} ${da}!`;
-}
 
-function mixedMessage() {
+document.getElementById('buttonClicker').onclick = function() {
+    let verb = action[Math.floor(Math.random() * 10)];
+    let adj = adjective[Math.floor(Math.random() * 10)];
+    let nou = noun[Math.floor(Math.random() * 10)];
+    let da = date[Math.floor(Math.random() * 4)]
     if(value === 0){
-        mixMini();
+        document.getElementById("wacky").innerHTML = `You will ${verb} ${adj} ${nou} ${da}!`;
+        document.getElementById("buttonClicker").innerHTML = 'Clear';
         value += 1;
         
     } else if (value === 1){
         value -= 1;
         document.getElementById("wacky").innerHTML = ' ';
-        
+        document.getElementById("buttonClicker").innerHTML = 'Click Me';
     }
     
 }
 
 
-
-buttonClick.addEventListener('click', mixedMessage);
 
